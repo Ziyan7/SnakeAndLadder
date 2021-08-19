c68bf15804b7d75accb185330d45c4b2822e5579
Your Name <you@example.com>
2021-08-19 18:11:47 +0530

    Added UC3

--- Game.java
+++ Game.java
@@ -2,6 +2,10 @@ package SnakeAndLadder;
 
 import java.util.Random;
 public class Game {
+	
+	public static final int NO_PLAY = 1;
+	public static final int  Ladder = 2;
+	public static final int  Snake = 3;
 
 	public static void main(String[] args) {
 		
@@ -11,8 +15,21 @@ public class Game {
 		
 		Random rand = new Random();
 		int die_number = rand.nextInt(7-1)+1;
+		System.out.println("Die Number :"+die_number);
 		
-
+		int choice = rand.nextInt(4-1)+1;
+				
+		switch(choice) 
+		{
+		        case NO_PLAY : System.out.println("NO PLAY");
+					           break;
+				case Ladder  : System.out.println("You inclined by "+die_number+" current position is "+pos);
+				               pos+=die_number;
+				               break;
+				case Snake   : System.out.println("You declined by "+die_number+" current position is"+pos); 
+					           pos-=die_number;
+							   break;
+		}
 
 	}
 
