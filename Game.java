a731960e90bcb86a4dc662a8af0cf07cd03775f5
Your Name <you@example.com>
2021-08-19 18:15:42 +0530

    Added UC4

--- Game.java
+++ Game.java
@@ -1,6 +1,7 @@
 package SnakeAndLadder;
 
 import java.util.Random;
+
 public class Game {
 	
 	public static final int NO_PLAY = 1;
@@ -9,14 +10,16 @@ public class Game {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to the Game");
+		System.out.println("Welcome to Game");
 		
 		int pos = 0;
 		
 		Random rand = new Random();
+		
+		while(pos != 100) {
 		int die_number = rand.nextInt(7-1)+1;
 		System.out.println("Die Number :"+die_number);
-		
+		 
 		int choice = rand.nextInt(4-1)+1;
 				
 		switch(choice) 
@@ -29,6 +32,12 @@ public class Game {
 				case Snake   : System.out.println("You declined by "+die_number+" current position is"+pos); 
 					           pos-=die_number;
 							   break;
+		}
+		
+		if(pos < 0) {
+			pos=0;
+		}
+		
 		}
 
 	}
