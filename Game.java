fa95711a7cf6f9a6f230bd4270f0954b02b0c401
Your Name <you@example.com>
2021-08-19 18:07:00 +0530

    Added UC2

--- Game.java
+++ Game.java
@@ -1,5 +1,6 @@
 package SnakeAndLadder;
 
+import java.util.Random;
 public class Game {
 
 	public static void main(String[] args) {
@@ -7,6 +8,11 @@ public class Game {
 		System.out.println("Welcome to the Game");
 		
 		int pos = 0;
+		
+		Random rand = new Random();
+		int die_number = rand.nextInt(7-1)+1;
+		
+
 
 	}
 
